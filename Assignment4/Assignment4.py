a3a7fd880ab547345222aaea77acdfee336ecf45
Thanabodin19 <thanabodin.kea@gmail.com>
2024-06-24 21:51:14 +0700

    add assignment4

---
+++ Assignment4/Assignment4.py
@@ -0,0 +1,57 @@
+import numpy as np
+import matplotlib.pyplot as plt
+DEBUG = 0
+# Normal Equation
+def normal_equation(X, y):
+    return np.linalg.inv(X.T.dot(X)).dot(X.T).dot(y)
+
+# Gradient Descent function
+def gradient_descent(X, y, learning_rate=0.5, n_iterations=1000):
+    m = len(y)
+    theta = np.zeros((X_b.shape[1]))  # initialization of theta
+    for _ in range(n_iterations):
+        gradients = X.T.dot(X.dot(theta) - y)
+        theta = theta - learning_rate / m * gradients
+        if (DEBUG):
+            print(f'X.dot(theta) = {X.dot(theta)}\n')
+            print(f'y = {y}\n')
+            print(f'X.dot(thea) - y = {X.dot(theta) - y}\n')
+            print(f'X = {X}\n')
+            print(f'X.T = {X.T}\n')
+            print(f'X.T.dot(X.dot(theta) - y) = {gradients}\n')
+            print(f'theta = {theta}\n')
+    return theta
+
+if __name__ == "__main__":
+    # Data points
+    X = np.array([0, 2])
+    y = np.array([0, 2])
+
+    # plt.scatter(X, y)
+    # X = (X - X.mean(axis=0)) / X.std(axis=0)
+    # Add x0 = 1 to each instance for the bias term
+    X_b = np.c_[np.ones((len(y), 1)), X]  # Add bias term
+    # print(X_b)
+
+    # Using Normal Equation
+    theta_normal = normal_equation(X_b, y)
+    print("Parameters from Normal Equation (theta):")
+    print(theta_normal)
+
+    # Using Gradient Descent
+    theta_gd = gradient_descent(X_b, y)
+    print("Parameters from Gradient Descent (theta):")
+    print(theta_gd)
+
+    # Predict using the estimated parameters
+    X_new = np.array([3, 7])
+    X_new_b = np.c_[np.ones((len(X_new), 1)), X_new]  # Add bias term
+    y_predict_normal = X_new_b.dot(theta_normal)
+    y_predict_gd = X_new_b.dot(theta_gd)
+
+    print(f"Predictions using Normal Equation:")
+    print(y_predict_normal)
+    print("Predictions using Gradient Descent:")
+    print(y_predict_gd)
+
+    plt.show()
